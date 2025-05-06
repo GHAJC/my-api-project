4ed24edc2effb3e2bd06f24bf0a579802ae6b273
GHAJC <112990359+GHAJC@users.noreply.github.com>
2025-05-06 17:05:39 -0400

    Update addOrUpdateProduct.js

--- api/addOrUpdateProduct.js
+++ api/addOrUpdateProduct.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://thefrozenshopfront.w3spaces-preview.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
