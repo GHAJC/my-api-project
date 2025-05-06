60fd399c149110aaac807bcbd7387b0a2f3b8aa5
GHAJC <112990359+GHAJC@users.noreply.github.com>
2025-05-06 18:07:00 -0400

    Update addOrUpdateProduct.js

--- api/addOrUpdateProduct.js
+++ api/addOrUpdateProduct.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
-export default async function handler(req, res) {
-  const allowedOrigins = ['https://thefrozenshopfront.w3spaces-preview.com', 'https://thefrozenshopfront.w3spaces.com'];
-const origin = req.headers.origin;
-if (allowedOrigins.includes(origin)) {
-  res.setHeader('Access-Control-Allow-Origin', origin);
-}
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://thefrozenshopfront.w3spaces.com/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end(); // Preflight response
