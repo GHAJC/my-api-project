499ff9f2fd70bea8274917ec9881807b2692fafc
GHAJC <112990359+GHAJC@users.noreply.github.com>
2025-05-06 18:12:19 -0400

    Update addOrUpdateProduct.js

--- api/addOrUpdateProduct.js
+++ api/addOrUpdateProduct.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
